78e2524f755a3dfb6647c68e8874312848489bd2
albert <ehrenberger_albert@yahoo.com>
2025-04-16 12:56:56 +0200

    Initial commit

---
+++ src/main/java/com/datix/coresystem_poc/CoresystemPocApplication.java
@@ -0,0 +1,13 @@
+package com.datix.coresystem_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoresystemPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoresystemPocApplication.class, args);
+		System.out.print("Hello World");
+	}
+}
