6205e60a3450f798786bb21134148e029b2bc688
albert <ehrenberger_albert@yahoo.com>
2025-05-22 23:55:12 +0200

    feature: client: added functional charging button

--- angularclient/src/app/components/charging/charging.component.ts
+++ angularclient/src/app/components/charging/charging.component.ts
@@ -1,7 +1,7 @@
 import { ChargingService } from './../../services/charging.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-charging',
@@ -9,13 +9,41 @@ import { Component, OnInit } from '@angular/core';
   imports: [CommonModule, HttpClientModule],
   styleUrl: './charging.component.scss'
 })
-export class ChargingComponent implements OnInit {
+export class ChargingComponent implements OnInit, OnDestroy {
+
+  clickCount = 0;
+
 
    constructor(private chargingService: ChargingService) {
     }
 
   ngOnInit() {
   this.chargingService.turnOn()
+  }
+
+  ngOnDestroy() {
+  this.chargingService.turnOff()
+  }
+
+  get buttonLabel(): string {
+  return this.clickCount % 2 === 0 ? 'Laden' : 'Stop Laden';
+  }
+
+  handleClick() {
+    this.clickCount++;
+    if (this.clickCount % 2 === 0) {
+      this.onEvenClick();
+    } else {
+      this.onOddClick();
+    }
+  }
+
+  onEvenClick() {
+  this.chargingService.remoteStop()
+  }
+
+  onOddClick() {
+  this.chargingService.remoteStart()
   }
 
 }
--- angularclient/src/app/services/charging.service.ts
+++ angularclient/src/app/services/charging.service.ts
@@ -13,11 +13,34 @@ export class ChargingService {
   }
 
   public turnOn(): void {
-    console.log(this.steveControllerUrl + "/turnOn")
     this.http.post(this.steveControllerUrl + "/turnOn", null)
     .subscribe({
       next: res => console.log('Turned on successfully', res),
       error: err => console.error('Failed to turn on:', err)
+    });
+  }
+
+  public turnOff(): void {
+    this.http.post(this.steveControllerUrl + "/turnOff", null)
+    .subscribe({
+      next: res => console.log('Turned off successfully', res),
+      error: err => console.error('Failed to turn off:', err)
+    });
+  }
+
+  public remoteStart(): void {
+    this.http.post(this.steveControllerUrl + "/remoteStart", null)
+    .subscribe({
+      next: res => console.log('remoteStart successfully', res),
+      error: err => console.error('Failed to remoteStart:', err)
+    });
+  }
+
+  public remoteStop(): void {
+    this.http.post(this.steveControllerUrl + "/remoteStop", null)
+    .subscribe({
+      next: res => console.log('remoteStop successfully', res),
+      error: err => console.error('Failed to remoteStop:', err)
     });
   }
 }
