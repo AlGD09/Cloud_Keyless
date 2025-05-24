97e2363bd555297f5eb3bef1f533713ef4562134
albert <ehrenberger_albert@yahoo.com>
2025-05-24 19:49:04 +0200

    feature: client: added barebones booking page

--- angularclient/src/app/app.routes.ts
+++ angularclient/src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
-import { BookingComponent } from './components/booking/booking.component';
+import { BookingComponentGPT } from './temp/booking.component';
 import { ChargingComponent } from './components/charging/charging.component';
 
 export const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'adduser', component: UserFormComponent },
   { path: '', redirectTo: 'buchen', pathMatch: 'full' },
-  { path: 'buchen', component: BookingComponent },
+  { path: 'buchen', component: BookingComponentGPT },
     { path: 'laden', component: ChargingComponent },
 ];
 
---
+++ angularclient/src/app/temp/booking.component.ts
@@ -0,0 +1,109 @@
+import { Wallbox, RentedWallbox } from './../model/wallbox';
+import { Component, OnInit } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { WallboxService } from '../services/wallbox.service';
+import { addMinutes, format, startOfDay } from 'date-fns';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@Component({
+  selector: 'app-booking',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule],
+  templateUrl: './booking.component.html',
+  styleUrls: ['./booking.component.scss']
+})
+export class BookingComponentGPT implements OnInit {
+  wallboxes: Wallbox[] = [];
+  selectedWallbox: any;
+  selectedDate: Date = new Date();
+  bookings: any[] = [];
+  timeSlots: any[] = [];
+  selectedSlots: Set<string> = new Set();
+
+  constructor(private wallboxService: WallboxService) {}
+
+  ngOnInit() {
+    this.loadWallboxes();
+  }
+
+  loadWallboxes() {
+    this.wallboxService.findAllRented().subscribe(rentedWallboxes => {
+      this.wallboxes = rentedWallboxes.map(rw => rw.wallbox);
+      this.selectedWallbox = this.wallboxes[0];
+      this.fetchBookings();
+    });
+  }
+
+onDateChange(event: MatDatepickerInputEvent<Date>) {
+  const selected = event.value;
+  console.log('Selected date:', selected);
+}
+
+  onWallboxChange(wallboxId: number) {
+    this.selectedWallbox = this.wallboxes.find(w => w.id === wallboxId);
+    this.fetchBookings();
+  }
+
+  fetchBookings() {
+    // this.wallboxService.getBookings(this.selectedWallbox.id).subscribe(data => {
+    //   this.bookings = data;
+    //   this.generateSlots();
+    // });
+  }
+
+  generateSlots() {
+    const slots = [];
+    const dayStart = startOfDay(this.selectedDate);
+    for (let hour = 7; hour <= 13; hour++) {
+      for (let min of [0, 15, 30, 45]) {
+        const slotStart = new Date(this.selectedDate.setHours(hour, min, 0, 0));
+        const slotKey = slotStart.toISOString();
+        const booked = this.bookings.some(booking =>
+          new Date(booking.startTime) <= slotStart && new Date(booking.endTime) > slotStart
+        );
+        slots.push({ time: slotStart, key: slotKey, booked });
+      }
+    }
+    this.timeSlots = slots;
+  }
+
+  toggleSlot(slot: any) {
+    if (slot.booked) return;
+    if (this.selectedSlots.has(slot.key)) {
+      this.selectedSlots.delete(slot.key);
+    } else {
+      this.selectedSlots.add(slot.key);
+    }
+  }
+
+  confirmBooking() {
+    // const sorted = Array.from(this.selectedSlots).sort();
+    // const start = new Date(sorted[0]);
+    // const end = addMinutes(new Date(sorted[sorted.length - 1]), 15);
+    // this.wallboxService.bookSlot(this.selectedWallbox.id, start, end).subscribe(() => {
+    //   this.selectedSlots.clear();
+    //   this.fetchBookings();
+    // });
+  }
+
+  isSelected(slotKey: string) {
+    return this.selectedSlots.has(slotKey);
+  }
+
+  getSelectedSlotRange(): string {
+  if (this.selectedSlots.size === 0) return '';
+
+  const sorted = Array.from(this.selectedSlots).sort();
+  const start = new Date(sorted[0]);
+  const end = new Date(sorted[sorted.length - 1]);
+  return `${start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - ${end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
+}
+}
