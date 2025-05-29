ebb56596959ed7284530772f6ae15ca84c92f123
albert <ehrenberger_albert@yahoo.com>
2025-05-29 16:42:26 +0200

    feature: client: display date in German locale

--- angularclient/src/main.ts
+++ angularclient/src/main.ts
@@ -1,13 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom, LOCALE_ID } from '@angular/core';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { appConfig } from './app/app.config';
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeDe);
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'de' } //Set German locale globally
   ]
 }).catch(err => console.error(err));
