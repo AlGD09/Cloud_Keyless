1182f55219994254ccd845665b72f959664dca1a
albert <ehrenberger_albert@yahoo.com>
2025-05-29 17:21:39 +0200

    feature: client: make InvoiceDialog use all Invoice data

--- angularclient/src/app/components/invoice-dialog/invoice-dialog.component.ts
+++ angularclient/src/app/components/invoice-dialog/invoice-dialog.component.ts
@@ -1,17 +1,25 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { UpcomingBooking } from '../../model/booking';
+import { Invoice } from '../../model/invoice';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
-  selector: 'app-booking-dialog',
+  selector: 'app-invoice-dialog',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatTableModule
+  ],
   templateUrl: './invoice-dialog.component.html',
   styleUrls: ['./invoice-dialog.component.scss']
 })
-
 export class InvoiceDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: UpcomingBooking) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Invoice) {}
+
+  timeSlotColumns: string[] = ['startTime', 'endTime', 'pings'];
+  transactionColumns: string[] = ['transactionId', 'startTime', 'endTime', 'wh'];
 }
